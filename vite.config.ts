9b275a893961771dd7762afa041de5af0026897f
Shak <sanshakurt@gmail.com>
2025-11-21 17:18:11 +0330

    implement tailwind config, logo, custom colors, packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
 
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
