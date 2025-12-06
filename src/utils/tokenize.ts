debdc61d0f605782358f6d6e8dfee1dbb5fe33ea
Shak <sanshakurt@gmail.com>
2025-12-06 15:27:49 +0330

    remove console.log

--- src/utils/tokenize.ts
+++ src/utils/tokenize.ts
@@ -18,8 +18,6 @@ export function tokenize(input: string): string[] {
   const parentheses = "()";
 
   for (const char of input) {
-    console.log(currentToken);
-
     // Skip spaces
     if (char === " ") {
       pushCurrentToken();
